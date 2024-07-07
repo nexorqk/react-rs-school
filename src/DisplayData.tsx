eb598a9c6c4f3ee71a2017ce2c2527d8f0d60e67
nexorqk <pkms2a10@gmail.com>
2024-07-08 02:28:22 +0300

    feat: add button to throw error

--- src/DisplayData.tsx
+++ src/DisplayData.tsx
@@ -6,15 +6,29 @@ export default class DisplayData extends Component<{
   data: DataResults[] | undefined
   isLoading: boolean
 }> {
+  state = {
+    isCustomError: false,
+  }
+
   render(): ReactNode {
     if (!this.props.data) {
       throw new Error('Error Display Data')
     }
 
+    if (this.state.isCustomError) {
+      throw new Error('Clicked Error Button')
+    }
+
     return (
       <>
-        {this.props.isLoading && <Loader />}
+        <button
+          className="errorButton"
+          onClick={() => this.setState({ isCustomError: true })}
+        >
+          Make Error
+        </button>
         <div className="bottom container">
+          {this.props.isLoading && <Loader />}
           {this.props.data?.map((character) => (
             <ul key={character.id} className="card">
               <li className="card-item">{character.name}</li>
--- src/ErrrorBoundary.tsx
+++ src/ErrrorBoundary.tsx
@@ -13,12 +13,10 @@ export default class ErrorBoundary extends Component<PropsType, StateType> {
   }
 
   componentDidCatch(error: Error, errorInfo: ErrorInfo) {
-    // Catch errors in any components below and re-render with error message
     this.setState({
       error: error,
       errorInfo: errorInfo,
     })
-    // You can also log error messages to an error reporting service here
   }
 
   render() {
